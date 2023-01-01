eb1cb4aa0101ecdc947b29245a7fde4241ce1ce9
MIcah GIles <micahdeang@gmail.com>
2023-01-01 15:43:56 -0600

    Add isBrowser check to the confetti animation

--- src/app/form-submitted/form-submitted.component/form-submitted.component.ts
+++ src/app/form-submitted/form-submitted.component/form-submitted.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
 import confetti from 'canvas-confetti';
 import { Subscription, take, timer } from 'rxjs';
@@ -9,21 +10,27 @@ import { Subscription, take, timer } from 'rxjs';
   styleUrls: ['./form-submitted.component.scss']
 })
 export class FormSubmittedComponent implements OnInit, OnDestroy {
+  private readonly isBrowser: boolean = isPlatformBrowser(this.platformId);
   private sub: Subscription = new Subscription();
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: any
+    ) { }
 
   ngOnInit(): void {
-    let count = 20;
-    this.sub.add(timer(20, 20).pipe(take(7)).subscribe(() => {
-      confetti({
-          angle: count,
-          spread: 60,
-          particleCount: 100,
-          origin: { y: 0.5 }
-      });
-      count += 25;
-    }));
+    if (this.isBrowser) {
+      let count = 20;
+      this.sub.add(timer(20, 20).pipe(take(7)).subscribe(() => {
+        confetti({
+            angle: count,
+            spread: 60,
+            particleCount: 100,
+            origin: { y: 0.5 }
+        });
+        count += 25;
+      }));
+    }
   }
 
   ngOnDestroy(): void {
