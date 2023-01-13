07dcb3365bbac341bbe859b1b16b784669b84aa4
MIcah GIles <micahdeang@gmail.com>
2023-01-12 20:14:31 -0600

    Start some unit tests on the form page, start writing README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
+To run this application locally you will need to have node and npm up to date on your machine.  
 
+To download Node: 
+    https://nodejs.org/en/
+
+npm documentation:
+    https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+
+I suggest making sure you have the most current version of these running on your machine.
+(For reference I am running npm version 9.2.0 and node 18.12.1)
+
+Of note, you may want to run the following commands with sudo for mac, or as admin in a command prompt to avoid issues. This may not be necessary however. 
+
+Once Node and npm are ready to go open a Terminal(Mac)/Command Prompt(Windows). 
+
+Then cd into the project folder and run "npm i" 
+
+To run SSR mode locally use 
+    npm run dev:ssr
--- src/app/profile-form/profile-form.component/profile-form.component.spec.ts
+++ src/app/profile-form/profile-form.component/profile-form.component.spec.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppModule } from 'src/app/app.module';
 import { FetchHttpService } from 'src/app/fetch-http.service/fetch-http.service';
 import { of } from 'rxjs/internal/observable/of';
+import { anyAlphaString, getOccupationsAndStates } from 'src/test-utils';
 
 describe('ProfileFormComponent', () => {
   ngMocks.faster();
@@ -23,9 +24,44 @@ describe('ProfileFormComponent', () => {
   });
 
   describe('ngOnInit', () => {
-    it('should get the occupations and states from the fetch servce', () => {
+    it('should get the occupations and states from the fetch service', () => {
+      MockInstance(FetchHttpService, 'getOccupationsAndStates', jest.fn())
+        .mockReturnValue(of(getOccupationsAndStates()));
       const fixture = render();
       
+      const fetchService = ngMocks.get(FetchHttpService);
+      expect(fetchService.getOccupationsAndStates).toHaveBeenCalledTimes(1);
+      expect(fetchService.getOccupationsAndStates).toHaveBeenCalledWith();
+    });
+
+    it('should display the occupations and states fetched from the service', () => {
+      const expectedStatesAndOccs = getOccupationsAndStates({
+        occupations: [
+          anyAlphaString(3),
+          anyAlphaString(4),
+          anyAlphaString(8)
+        ],
+        states: [
+          { name: anyAlphaString(6), abbreviation: anyAlphaString(2) },
+          { name: anyAlphaString(6), abbreviation: anyAlphaString(2) }
+        ]
+      });
+      MockInstance(FetchHttpService, 'getOccupationsAndStates', jest.fn())
+        .mockReturnValue(of(expectedStatesAndOccs));
+      const fixture = render();
+
+      const states = ngMocks.find(fixture, '[data-testid=state-selector]').children;
+      const occs = ngMocks.find(fixture, '[data-testid=occ-selector]').children;
+      
+      expect(states.length).toEqual(2);
+      expect(states[0].nativeElement.text).toEqual(expectedStatesAndOccs.states[0].name);
+      expect(states[1].nativeElement.text).toEqual(expectedStatesAndOccs.states[1].name);
+      expect(occs.length).toEqual(3);
+      expect(occs[0].nativeElement.text).toEqual(expectedStatesAndOccs.occupations[0]);
+      expect(occs[1].nativeElement.text).toEqual(expectedStatesAndOccs.occupations[1]);
+      expect(occs[2].nativeElement.text).toEqual(expectedStatesAndOccs.occupations[2]);
     });
   });
+
+  // More unit tests should be written here, but I'm going to call it a day
 });
